7c4cc2741f60fe2ea164f89dd3e262d49f3394f8
someone1351 <184540170+someone1351@users.noreply.github.com>
2024-10-17 17:38:54 +1100

    Add files via upload

--- src/def/mod.rs
+++ src/def/mod.rs
@@ -5,7 +5,6 @@ pub mod branch;
 use std::any::{Any, TypeId};
 use std::collections::HashMap;
 
-// use std::ops::{Bound, Range, RangeBounds};
 use std::str::FromStr;
 
 use branch::*;
@@ -21,12 +20,6 @@ pub struct Def {
     for_tag_names : Vec<String>,
     cur_nodes_start : usize,
     tags_once:bool,
-
-
-    // cur_repeat_start:Option<usize>,
-    // cur_params_num:usize,
-    // cur_optional:Option<usize>,
-    // cur_group_param_start:Option<usize>,
 }
 
 impl Def {
@@ -38,20 +31,21 @@ impl Def {
             for_tag_names : Vec::new(),
             cur_nodes_start : 0,
             tags_once:false,
-            // cur_repeat_start:None,
-            // cur_params_num:0,
-            // cur_group_param_start:None,
         }
     }
 
+    pub fn get_branch(&self, branch_name : &str) -> Option<BranchContainer> {
+        self.branch_map.get(branch_name).map(|&branch_ind|BranchContainer {
+            def:self,
+            branch_ind,
+        })
+    }
 
     pub fn branch(mut self, branch_name : &str) -> Self {
         if self.branch_map.contains_key(branch_name) { //panic or overwrite?
             panic!("Branch label already used.")
         }
-        // if self.cur_group_param_start.is_some() {
-        //     panic!("confdef, group not ended");
-        // }
+
         //
         self.cur_nodes_start = self.nodes.len();
         let branch_index = self.branches.len();
@@ -63,53 +57,33 @@ impl Def {
     }
 
     pub fn insert_nodes(mut self, branch_name : &str) -> Self { //from branch_nodes_from
-        // let cur_branch = self.cur_branch_mut();
-        
         //
         if self.branches.len()==0 { //panic or do nothing?
             panic!("confdef, no branch available!");
         }
-        // if self.cur_group_param_start.is_some() {
-        //     panic!("confdef, group not ended");
-        // }
-        // if let Some(branch_rest_ind)=self.branch_rest_ind {
-        //     if branch_rest_ind+1==self.branches.len() {
-        //         panic!("confdef, cannot insert nodes into rest branch");
-        //     }
-        // }
 
         //
         let cur_branch = self.branches.last_mut().unwrap();
         cur_branch.branch_inserts.push(branch_name.to_string());
         self.cur_nodes_start = self.nodes.len();
         self.tags_once=false;
+
+        //
         self
     }
     
     pub fn tagless_nodes(mut self, ) -> Self {
-
         //
         if self.branches.len()==0 { //panic or do nothing?
             panic!("confdef, no branch available!");
         }
-        // if self.cur_group_param_start.is_some() {
-        //     panic!("confdef, group not ended");
-        // }
-        // if let Some(branch_rest_ind)=self.branch_rest_ind {
-        //     if branch_rest_ind+1==self.branches.len() {
-        //         panic!("confdef, cannot add nodes into rest branch");
-        //     }
-        // }
 
         //
         self.cur_nodes_start = self.nodes.len(); //makes modifying a node an error if each hasnt been called?
         self.for_tag_names.clear();
-        // self.for_tag_valstr_key=0;
         self.tags_once=false;
 
-        // self.cur_params_num=0;
-        // self.cur_repeat_start=None;
-
+        //
         self
     }
 
@@ -117,41 +91,30 @@ impl Def {
     where
         T:IntoIterator<Item = &'t str>,
     {
+        //if no param given to a tagless node, it will just have a node with no params, that will be skipped over during parsing
+        
         //
         if self.branches.len()==0 { //panic or do nothing?
             panic!("confdef, no branch available!");
         }
 
-        // if self.cur_group_param_start.is_some() {
-        //     panic!("confdef, group not ended");
-        // }
-        // if let Some(branch_rest_ind)=self.branch_rest_ind {
-        //     if branch_rest_ind+1==self.branches.len() {
-        //         panic!("confdef, cannot add nodes into rest branch");
-        //     }
-        // }
-
         //
         self.cur_nodes_start = self.nodes.len(); //makes modifying a node an error if each hasnt been called?
-        self.for_tag_names.clear();
-        
+        self.for_tag_names.clear();        
         self.for_tag_names.extend(tag_names.into_iter().map(|x|x.to_string()));
-        // self.for_tag_valstr_key = val_type_ind;
-
         self.tags_once=once;
 
-        // self.cur_params_num=0;
-        // self.cur_repeat_start=None;
-
+        //
         self
     }
-    
+
     pub fn tag_nodes<'t,T>(self, tag_names: T) -> Self 
     where
         T:IntoIterator<Item = &'t str>,
     {
         self.inner_tag_nodes(tag_names,false)
     }
+
     pub fn tag_nodes_once<'t,T>(self, tag_names: T) -> Self 
     where
         T:IntoIterator<Item = &'t str>,
@@ -166,20 +129,12 @@ impl Def {
             panic!("confdef, no branch available!");
         }
 
-        // if self.cur_group_param_start.is_some() {
-        //     panic!("confdef, group not ended");
-        // }
-
         //
         let branch_ind=self.branches.len()-1;
         let cur_branch = self.branches.last_mut().unwrap();
 
-
         //
         self.cur_nodes_start = self.nodes.len();
-        // self.cur_params_num=0;
-        // self.cur_repeat_start=None;
-        // self.cur_group_param_start=None;
 
         if self.for_tag_names.len()==0 {
             let node_index = self.nodes.len();
@@ -206,137 +161,77 @@ impl Def {
         }
     }
 
-    pub fn entry(mut self) -> Self {
+    pub fn entry(mut self,
+        label : Option<&str>,
+        children : Option<&str>,
+    ) -> Self {
         self.inner_entry();
+
+        for node_index in self.cur_nodes_start .. self.nodes.len() {
+            let node=self.nodes.get_mut(node_index).unwrap();
+            node.node_label=label.map(|x|x.to_string());
+
+            if let Some(children)=children {
+                node.children = NodeChildren::Branch(children.to_string());
+            }
+        }
+
         self
     }
-    pub fn rentry(mut self) -> Self {
+
+    pub fn entry_text(mut self,
+        label : Option<&str>,
+    ) -> Self {
         self.inner_entry();
-        
+
         for node_index in self.cur_nodes_start .. self.nodes.len() {
             let node=self.nodes.get_mut(node_index).unwrap();
-            node.rsimilar=true;
-        }
-
-        self
-    }
-    // pub fn priority(mut self,priority:i32) -> Self {
-    //     if self.cur_nodes_start==self.nodes.len() {
-    //         panic!("confdef, no nodes");
-    //     }
-
-    //     for node_index in self.cur_nodes_start .. self.nodes.len() {
-    //         let node=self.nodes.get_mut(node_index).unwrap();
-    //         let last_param_group=node.param_groups.last_mut().unwrap();
-
-    //         // if last_param_group.greedy {
-    //         //     panic!("confdef, greedy already set");
-    //         // }
-
-    //         last_param_group.priority=priority;
-    //     }
-        
-    //     self
-    // }
-
-    pub fn optional(mut self) -> Self {
-        if self.cur_nodes_start==self.nodes.len() {
-            panic!("confdef, no nodes");
-        }
-
-        // if self.cur_params_num==0 {
-        //     panic!("confdef, no param to make optional");
-        // }
-
-        // if self.cur_group_param_start.is_some() {
-        //     panic!("confdef, cannot use optional inside group");
-        // }
-        
-        for node_index in self.cur_nodes_start .. self.nodes.len() {
-            
-            let node=self.nodes.get_mut(node_index).unwrap();
-
-            // if node.params.is_empty() {
-            //     panic!("confdef, no param to make optional");
-            // }
-            
-            // Self::inner_init_param(node);
-
-            let last_param_group=node.param_groups.last_mut().unwrap();
-
-            if last_param_group.optional {
-                panic!("confdef, optional already set");
-            }
-
-            last_param_group.optional=true;
-        }
-
-        self
-    }
-
-    pub fn repeat(mut self) -> Self {
-        if self.cur_nodes_start==self.nodes.len() {
-            panic!("confdef, no nodes");
-        }
-
-        // if self.cur_params_num==0 {
-        //     panic!("confdef, no param to make repeat");
-        // }
-        
-        // if self.cur_group_param_start.is_some() {
-        //     panic!("confdef, cannot use repeat inside group");
-        // }
-        
-        for node_index in self.cur_nodes_start .. self.nodes.len() {
-            
-            let node=self.nodes.get_mut(node_index).unwrap();
-
-            // Self::inner_init_param(node);
-            // if node.params.is_empty() {
-            //     panic!("confdef, no param to make repeat");
-            // }
-
-            let last_param_group=node.param_groups.last_mut().unwrap();
-
-            if last_param_group.repeat {
-                panic!("confdef, repeat already set");
-            }
-
-            last_param_group.repeat=true;
+            node.node_label=label.map(|x|x.to_string());
+            node.children = NodeChildren::Body(None);
         }
 
         self
     }
     
-    pub fn name(mut self, param_group_name:&str) -> Self {
-        if self.cur_nodes_start==self.nodes.len() {
-            panic!("confdef, no nodes");
-        }
-
-        // if self.cur_params_num==0 {
-        //     panic!("confdef, no params");
-        // }
-                
-        for node_index in self.cur_nodes_start .. self.nodes.len() {            
+    pub fn rentry(mut self,
+        label : Option<&str>,
+        children : Option<&str>,
+    ) -> Self {
+        self.inner_entry();
+        
+        for node_index in self.cur_nodes_start .. self.nodes.len() {
             let node=self.nodes.get_mut(node_index).unwrap();
+            node.rsimilar=true;
+            node.node_label=label.map(|x|x.to_string());
 
-            if node.param_groups.is_empty() {
-                panic!("confdef, no param to name");
+            if let Some(children)=children {
+                node.children = NodeChildren::Branch(children.to_string());
             }
-
-            let last_param=node.param_groups.last_mut().unwrap();
-
-            if last_param.name.is_some() {
-                panic!("confdef, param already named");
-            }
-
-            last_param.name=Some(param_group_name.to_string());
         }
 
         self
     }
 
-    pub fn group(mut self) -> Self {
+    pub fn rentry_text(mut self,
+        label : Option<&str>,
+    ) -> Self {
+        self.inner_entry();
+        
+        for node_index in self.cur_nodes_start .. self.nodes.len() {
+            let node=self.nodes.get_mut(node_index).unwrap();
+            node.rsimilar=true;
+            node.node_label=label.map(|x|x.to_string());
+            node.children = NodeChildren::Body(None);
+        }
+
+        self
+    }
+
+    pub fn group(mut self,
+        name:Option<&str>,
+        optional:bool,
+        repeat:bool,    
+    ) -> Self {
         if self.cur_nodes_start==self.nodes.len() {
             panic!("confdef, no nodes");
         }
@@ -344,100 +239,21 @@ impl Def {
         for node_index in self.cur_nodes_start .. self.nodes.len() {
             let node=self.nodes.get_mut(node_index).unwrap();
             
-            if !node.param_groups.last().unwrap().specified { //.params.is_empty()
-                // if !node.param_groups.last().unwrap().params.is_empty() { //not really needed, just force user to either use groups or not at all
-                //     panic!("confdef, cannot add group when params added previously without group");
-                // }
-
-                //
+            if !node.param_groups.last().unwrap().specified {
                 node.param_groups.pop().unwrap();
             }
 
             node.param_groups.push(ParamGroup{specified:true,..Default::default()});
-        }
 
-        // self.cur_params_num+=1;
+            let last_param_group=node.param_groups.last_mut().unwrap();
 
-        self
-    }
-
-
-    pub fn children(mut self, children_branch :&str) -> Self {
-        if self.cur_nodes_start==self.nodes.len() {
-            panic!("confdef, no nodes");
-        }
-
-        for node_index in self.cur_nodes_start .. self.nodes.len() {
-            let node=self.nodes.get_mut(node_index).unwrap();
-
-            if node.children != NodeChildren::None {
-                panic!("confdef, children already set");
-            }
-            
-            node.children = NodeChildren::Branch(children_branch.to_string());
+            last_param_group.repeat=repeat;
+            last_param_group.optional=optional;
+            last_param_group.name=name.map(|x|x.to_string());
         }
 
         self
     }
-
-    pub fn text(mut self, ) -> Self { //text_node_label : Option<&str>
-        if self.cur_nodes_start==self.nodes.len() {
-            panic!("confdef, no nodes");
-        }
-        
-        for node_index in self.cur_nodes_start .. self.nodes.len() {
-            let node=self.nodes.get_mut(node_index).unwrap();
-
-            if node.children != NodeChildren::None {
-                panic!("confdef, children already set");
-            }
-
-            node.children = NodeChildren::Body(None); //text_node_label.map(|x|x.to_string())
-        }
-
-        self
-    }
-
-    pub fn label(mut self, node_label : &str) -> Self {
-        if self.cur_nodes_start==self.nodes.len() {
-            panic!("confdef, no nodes");
-        }
-        
-        for node_index in self.cur_nodes_start .. self.nodes.len() {
-            let node=self.nodes.get_mut(node_index).unwrap();
-
-            if node.node_label.is_some() {
-                panic!("confdef, label already set");
-            }
-            node.node_label=Some(node_label.to_string());
-        }
-
-        self
-    }
-
-    // pub fn rsimilar(mut self) -> Self {
-    //     if self.cur_nodes_start==self.nodes.len() {
-    //         panic!("confdef, no nodes");
-    //     }
-
-    //     for node_index in self.cur_nodes_start .. self.nodes.len() {
-    //         let node=self.nodes.get_mut(node_index).unwrap();
-
-    //         if node.rsimilar {
-    //             panic!("confdef, rsimilar already set");
-    //         }
-
-    //         node.rsimilar=true;
-    //     }
-
-    //     self
-    // }
-
-    // fn inner_init_param(node:&mut Node) {
-    //     if node.param_groups.is_empty() {
-    //         node.param_groups.push(ParamGroup { optional: false, repeat: false, name: None, params: Vec::new() });
-    //     }
-    // }
 
     fn inner_add_param_item(&mut self,param_item:Param) {
         if self.cur_nodes_start==self.nodes.len() {
@@ -445,7 +261,7 @@ impl Def {
         }
         
         //calc len of any repeating patterns in the param group eg (int bool int bool) => (int bool) => 2
-        //bit inefficent to recalc everytime a param is added, should do it once they are all added
+        //bit inefficent to recalc patterns everytime a param is added, should instead do it once they are all added
         let (pattern_len,patterns_num)={
             let mut params=self.nodes.last().unwrap()
                 .param_groups.last().unwrap()
@@ -502,9 +318,6 @@ impl Def {
         }
     }
 
-    //if no param given to a tagless node, it will just have a node with no params, that will be skipped over during parsing
-    
-    
     pub fn any(mut self) -> Self { //str, any
         self.inner_add_param_item(None);
         self
@@ -514,20 +327,10 @@ impl Def {
     where
         T:FromStr+Any+Send+Sync,
     {
-        //std::any::type_name::<T>(),
         let func2=|s:&str|T::from_str(s).ok().map(|p|Box::new(p) as Box<dyn Any+Send+Sync>);
         let param_item:Param=Some((TypeId::of::<T>(),std::any::type_name::<T>(),func2));
         self.inner_add_param_item(param_item);
         
         self
     }
-    
-    pub fn get_branch(&self, branch_name : &str) -> Option<BranchContainer> {
-        self.branch_map.get(branch_name).map(|&branch_ind|BranchContainer {
-            def:self,
-            branch_ind,
-        })
-    }
-
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,34 @@
-
-
 use std::{collections::HashMap, path::PathBuf};
 
 use conf_def::{ Conf, RecordContainer, Walk};
 
-// mod conf_def;
-
-fn get_record_info(walk:&Walk) -> String {
-    let record=walk.record();
-    format!("{} {}{}: {}[{:}]{} @ ({}:{}:{})",
-        walk.is_exit().then_some("<=").unwrap_or("=>"),
-        "   ".repeat(walk.depth()),
-        walk.order(),
-        record.tag().map(|x|format!("{x:?} : ")).unwrap_or_default(),
-        record.values().map(|x|format!("{:?}",x.as_str())).collect::<Vec<_>>().join(", "),
-        record.has_text().then(||format!(" : {:?} :",record.text_values().map(|x|x.as_str()).collect::<Vec<_>>().join("\n"))).unwrap_or_default(),
-        record.branch_name().map(|x|format!("{x}")).unwrap_or("_".to_string()),
-        record.node_label().map(|x|format!("{x}")).unwrap_or("_".to_string()),
-        record.path().map(|x|format!("{x:?}")).unwrap_or("_".to_string()),
-    )
-}
-
-fn get_group_vals_info(record:RecordContainer) -> String {
-    format!("{}",
-        (0..record.param_groups_num()).map(|param_group_ind|{
-            let param_group=record.param_group(param_group_ind);
-            format!("{}:{}",
-                param_group.name().map(|x|format!("{x:?}")).unwrap_or("_".to_string()), {
-                let x=(0..param_group.many_num()).map(|many_ind|{
-                    let y=(0..param_group.params_num())
-                        .map(|param_ind|param_group.value(many_ind*param_group.params_num()+param_ind).as_str())
-                        .collect::<Vec<_>>().join(", ");
-                    if param_group.params_num()==1 {format!("{y}",)} else {format!("({y})",)}
-                }).collect::<Vec<_>>().join(", ");
-                if !param_group.is_repeat() {format!("{x}",)} else {format!("[{x}]",)}
-            })
-        }).collect::<Vec<_>>().join(", ")
-    )
-}
-
 fn create_def() -> conf_def::Def {
     conf_def::Def::new()
         .branch("root_branch")
-            .tag_nodes(["text"]).entry().text()
-            .tag_nodes(["include"]).entry().any()
+            .tag_nodes(["text"])
+                .entry_text(None) 
+            .tag_nodes(["include"])
+                .entry(None,None)
+                    .any()
             .tag_nodes(["hello"])
-                .rentry().children("root_branch")
-                    .group().name("ints").repeat() .optional()
+                .rentry(None,Some("root_branch"))
+                    .group(Some("ints"),true,true) 
                         .parse::<i32>()
                         .parse::<i32>()
-                    .group().name("the opt int").optional()
+                    .group(Some("the opt int"),true,false)
                         .parse::<i32>()
-                    .group().name("the int")
+                    .group(Some("the int"),false,false)
                         .parse::<i32>()
                         .parse::<i32>()
-                    .group().name("the any").any()
+                    .group(Some("the any"),false,false)
+                        .any()
             .tagless_nodes()
-                .entry().label("somevals").repeat()
-                    .parse::<i32>()
+                .entry(Some("somevals"), None)
+                    .group(None,false,true)
+                        .parse::<i32>()
 
         //     .insert_nodes("rest_branch")
-        // .branch("rest_branch").tagless_nodes().entry().children("rest").label("rest").repeat().any()
-}
-
-fn load_confs(def:conf_def::Def) -> HashMap<PathBuf, (Conf,String)> {
-    let root_branch=def.get_branch("root_branch").unwrap();
-
-    let file_paths = Vec::from_iter(std::fs::read_dir("examples").unwrap().filter_map(Result::ok)
-        .map(|e| e.path())
-        .filter(|p| p.extension() == Some(std::ffi::OsStr::new("conf"))),
-    );
-
-    let mut confs=HashMap::new();
-
-    for file_path in file_paths {
-        let src = std::fs::read_to_string(file_path.as_path()).unwrap();
-
-        match root_branch.parse(src.as_str(), true,Some(file_path.as_path())) {
-            Ok(conf)=>{
-                confs.insert(file_path, (conf,src));
-            }
-            Err(e)=>{
-                println!("{}",e.msg(Some(src.as_str())));
-            }
-        }
-    }
-
-    confs
+        // .branch("rest_branch").tagless_nodes().entry(Some("rest"),Some("rest_branch")).group(None, false, true).any()
 }
 
 fn walk_test() {
@@ -129,7 +71,65 @@ fn walk_test() {
     }
 }
 
-
 fn main() {
     walk_test();
+}
+
+fn load_confs(def:conf_def::Def) -> HashMap<PathBuf, (Conf,String)> {
+    let root_branch=def.get_branch("root_branch").unwrap();
+
+    let file_paths = Vec::from_iter(std::fs::read_dir("examples").unwrap().filter_map(Result::ok)
+        .map(|e| e.path())
+        .filter(|p| p.extension() == Some(std::ffi::OsStr::new("conf"))),
+    );
+
+    let mut confs=HashMap::new();
+
+    for file_path in file_paths {
+        let src = std::fs::read_to_string(file_path.as_path()).unwrap();
+
+        match root_branch.parse(src.as_str(), true,Some(file_path.as_path())) {
+            Ok(conf)=>{
+                confs.insert(file_path, (conf,src));
+            }
+            Err(e)=>{
+                println!("{}",e.msg(Some(src.as_str())));
+            }
+        }
+    }
+
+    confs
+}
+
+fn get_record_info(walk:&Walk) -> String {
+    let record=walk.record();
+    format!("{} {}{}: {}[{:}]{} @ ({}:{}:{})",
+        walk.is_exit().then_some("<=").unwrap_or("=>"),
+        "   ".repeat(walk.depth()),
+        walk.order(),
+        record.tag().map(|x|format!("{x:?} : ")).unwrap_or_default(),
+        record.values().map(|x|format!("{:?}",x.as_str())).collect::<Vec<_>>().join(", "),
+        record.has_text().then(||format!(" : {:?} :",record.text_values().map(|x|x.as_str()).collect::<Vec<_>>().join("\n"))).unwrap_or_default(),
+        record.branch_name().map(|x|format!("{x}")).unwrap_or("_".to_string()),
+        record.node_label().map(|x|format!("{x}")).unwrap_or("_".to_string()),
+        record.path().map(|x|format!("{x:?}")).unwrap_or("_".to_string()),
+    )
+}
+
+fn get_group_vals_info(record:RecordContainer) -> String {
+    format!("{}",
+        (0..record.param_groups_num()).map(|param_group_ind|{
+            let param_group=record.param_group(param_group_ind);
+            format!("{}:{}",
+                param_group.name().map(|x|format!("{x:?}")).unwrap_or("_".to_string()), {
+                let x=(0..param_group.many_num()).map(|many_ind|{
+                    let y=(0..param_group.params_num())
+                        .map(|param_ind|param_group.value(many_ind*param_group.params_num()+param_ind).as_str())
+                        .collect::<Vec<_>>().join(", ");
+                    if param_group.params_num()==1 {format!("{y}",)} else {format!("({y})",)}
+                }).collect::<Vec<_>>().join(", ");
+                if !param_group.is_repeat() {format!("{x}",)} else {format!("[{x}]",)}
+            })
+        }).collect::<Vec<_>>().join(", ")
+    )
 }
